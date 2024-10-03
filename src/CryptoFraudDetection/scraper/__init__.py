2b4ab6277394accc18dd7d536fc3c17da366f320
Gabriel Torres Gamez <gabriel.torres@fuet.ch>
2024-10-03 15:44:21 +0200

    rename x in init

--- src/CryptoFraudDetection/scraper/__init__.py
+++ src/CryptoFraudDetection/scraper/__init__.py
@@ -4,6 +4,6 @@ import CryptoFraudDetection.scraper.google_results
 import CryptoFraudDetection.scraper.google_trends
 import CryptoFraudDetection.scraper.reddit
 import CryptoFraudDetection.scraper.telegram
-import CryptoFraudDetection.scraper.x
+import CryptoFraudDetection.scraper.twitter
 
 import CryptoFraudDetection.scraper.utils
