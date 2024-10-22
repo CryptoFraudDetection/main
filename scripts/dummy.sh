1e9176dde95d6a03223c7049b5985942f2d97bf1
Nodøn <me@nodon.io>
2024-10-22 13:58:31 +0200

    Slurm batch accepts sweep id
    If sweep id is passed, then an agent is added to the existing sweep and a new sweep and agent are started otherwise.

--- scripts/dummy.sh
+++ scripts/dummy.sh
@@ -5,4 +5,12 @@
 #SBATCH -J dummy_model
 #SBATCH -o dummy_out.log
 #SBATCH -e dummy_err.log
-.venv/bin/python scripts/dummy.py
+
+# Check if sweep ID is provided as an argument
+if [ -z "$1" ]; then
+    # No sweep ID passed, run the initial command
+    .venv/bin/python scripts/dummy.py
+else
+    # Sweep ID passed, add agent to existing sweep
+    .venv/bin/python scripts/dummy.py --sweep_id "$1"
+fi
