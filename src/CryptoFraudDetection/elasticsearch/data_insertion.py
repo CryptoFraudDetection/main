f48ad06112e7518627b651a06aacab563edc7be7
can <canelian.barth@students.fhnw.ch>
2024-11-06 10:30:00 +0100

    merged main into branche

--- src/CryptoFraudDetection/elasticsearch/data_insertion.py
+++ src/CryptoFraudDetection/elasticsearch/data_insertion.py
@@ -41,3 +41,33 @@ def insert_dataframe(
     except BulkIndexError as e:
         logger.info(f"Skipped some documents:\n{e}")
     return success_fail
+
+
+def insert_dict(
+    logger: logging.Logger, index: str, data_dict: List[Dict[str, Any]]
+) -> Tuple[int, int | List[Dict[str, Any]]]:
+    """
+    Insert a list of dictionaries into Elasticsearch.
+
+    Args:
+    - index (str): The Elasticsearch index to insert the data into.
+    - data_dict (List[Dict[str, Any]]): The list of dictionaries to insert.
+
+    Returns:
+    - Tuple[int, int | List[Dict[str, Any]]]: Elasticsearch bulk insert response.
+    """
+    if any('id' in record for record in data_dict):
+        data = [
+            {"_index": index, "_id": record['id'], "_op_type": 'create', "_source": record} for record in data_dict
+        ]
+    else:
+        data = [
+            {"_index": index, "_source": record} for record in data_dict
+        ]
+    try:
+        success_fail = bulk(client=es, actions=data, raise_on_error=False)
+    except BulkIndexError as e:
+        logger.info(f"Skipped some documents:\n{e}")
+        success_fail = (0, 0)
+    return success_fail
+
