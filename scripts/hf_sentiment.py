6e1b6020dd78ec8ff0632f6663e5c3f894456742
Nodøn <me@nodon.io>
2024-12-17 11:49:34 +0100

    call hf_sentiment from project level dir

--- scripts/hf_sentiment.py
+++ scripts/hf_sentiment.py
@@ -8,10 +8,10 @@ from CryptoFraudDetection.utils import logger
 
 LOGGER = logger.Logger(name=__name__, level=enums.LoggerMode.INFO, log_dir="../logs")
 
-reddit_parquet_path = "../data/processed/reddit_embedded.parquet"
-sentiment_reddit_parquet_path = "../data/processed/reddit_sentiment.parquet"
-twitter_parquet_path = "../data/processed/x_embedded.parquet"
-sentiment_twitter_parquet_path = "../data/processed/x_sentiment.parquet"
+reddit_parquet_path = "data/processed/reddit_embedded.parquet"
+sentiment_reddit_parquet_path = "data/processed/reddit_sentiment.parquet"
+twitter_parquet_path = "data/processed/x_embedded.parquet"
+sentiment_twitter_parquet_path = "data/processed/x_sentiment.parquet"
 
 if not Path(sentiment_reddit_parquet_path).exists():
     df = pd.read_parquet(reddit_parquet_path)
